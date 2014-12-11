a0c01428252b6306a725cb3f470ed5d006779dfd
João Neto <joaopintoneto@gmail.com>
2014-12-11 02:00:46 -0200

    fix all to run on localhost

--- manager/scripts/utils/vsoft.js
+++ manager/scripts/utils/vsoft.js
@@ -23,8 +23,8 @@ angular.module('framework.vsoft', ['ui.vsoft', 'core.vsoft'])
         deviceId: (window.device) ? device.uuid.toLowerCase() : 'what.do.namehihi????',
         defaultPass: 'defaultPassword',
         name: 'iTaxi',
-        apiHost: 'http://chris-ictu.tk:6868', // taxigo.vn:997
-        mediaHost: 'http://vsoft.vn:1235'
+        apiHost: 'http://localhost:6868', // taxigo.vn:997
+        mediaHost: 'http://localhost:6868'
     });
 
 
